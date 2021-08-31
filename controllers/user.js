4a59b18adac9bce4d8dcfad523592b9c7344661e
jorgeramirez <Jorge.Ramirez.Sanchez@gt.ey.com>
2021-08-31 16:04:03 -0600

    Insertar datos usuario

--- controllers/user.js
+++ controllers/user.js
@@ -2,16 +2,25 @@ const Sequelize     = require('sequelize');
 const usuario       = require('../models').user;
 module.exports = {
  create2(req, res) {
- 
+    
     return usuario
         .create ({
             firstName: req.body.firstName,
-            lastName: req.body.lastName
-
+            lastName: req.body.lastName,
+            birthday: req.body.birthday,
+            email: req.body.email,
+            dni: req.body.dni,
+            phoneNumber: req.body.phoneNumber,
+            address: req.body.address,
+            state: req.body.state,
+            town: req.body.town,
+            type: req.body.type,
+            password: req.body.password,
              
         })
         .then(usuario => res.status(200).send(usuario))
         .catch(error => res.status(400).send(error))
+         console.log(req)
  },
    
  create(req, res) {
